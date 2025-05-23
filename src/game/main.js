f43c9157ab13817e7e87735db5e99f32624001a4
ianjuly4 <ianjuly4@gmail.com>
2025-05-23 12:39:31 -0600

    main menu update

--- src/game/main.js
+++ src/game/main.js
@@ -1,30 +1,38 @@
-import { Boot } from './scenes/Boot';
+
 import { Game as MainGame } from './scenes/Game';
 import { GameOver } from './scenes/GameOver';
 import { MainMenu } from './scenes/MainMenu';
-import { Preloader } from './scenes/Preloader';
+
 import { AUTO, Game } from 'phaser';
+import { SelectCharacter } from './scenes/selectCharacter';
 
 //  Find out more information about the Game Config at:
 //  https://docs.phaser.io/api-documentation/typedef/types-core#gameconfig
 const config = {
     type: AUTO,
-    width: 1024,
-    height: 768,
     parent: 'game-container',
-    backgroundColor: '#028af8',
     scale: {
         mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_BOTH
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: 1204,
+        height: 600
+    },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 200 },
+            enableBody: true,
+            //debug: true
+        }
     },
     scene: [
-        Boot,
-        Preloader,
         MainMenu,
         MainGame,
-        GameOver
+        GameOver,
+        SelectCharacter,
     ]
 };
+
 
 const StartGame = (parent) => {
 
--- src/game/scenes/MainMenu.js
+++ src/game/scenes/MainMenu.js
@@ -7,14 +7,58 @@ export class MainMenu extends Scene
         super('MainMenu');
     }
 
+    preload(){
+        //backgrounds
+        this.load.image('clouds', 'assets/backgrounds/nature_3/1.png');
+        this.load.image('mountain', 'assets/backgrounds/nature_3/2.png');
+        this.load.image('checkered', 'assets/backgrounds/nature_3/3.png');
+        this.load.image('trees_and_grass_foreground', 'assets/backgrounds/nature_3/4.png')
+    }
+
     create ()
     {
-        this.add.image(512, 384, 'background');
+       const {width, height} = this.scale
 
-        this.add.image(512, 300, 'logo');
+        this.bgClouds = this.add.tileSprite(0,0,width, height,'clouds')
+            .setOrigin(0,0)
+            .setDepth(-2)
+           
 
-        this.add.text(512, 460, 'Main Menu', {
+        this.add.image(0,0, 'mountain')
+            .setOrigin(0,0)
+            .setDepth(-1)
+            .setDisplaySize(width, height)
+
+        this.add.image(0,0,'trees_and_grass_foreground' )
+            .setOrigin(0,0)
+            .setDepth(0)
+            .setDisplaySize(width, height)
+        
+
+        this.add.text(512, 160, 'Monstrum: The Cursed Hero', {
             fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
+            stroke: '#000000', strokeThickness: 8,
+            align: 'center'
+        }).setOrigin(0.5);
+        
+        const newGameText = this.add.text(512, 260, 'New Game', {
+            fontFamily: 'Arial Black', fontSize: 28, color: '#ffffff',
+            stroke: '#000000', strokeThickness: 8,
+            align: 'center'
+        }).setOrigin(0.5).setInteractive();
+
+        newGameText.on('pointerdown', ()=>{
+            this.scene.start('SelectCharacter')
+        })
+
+        this.add.text(512, 340, "Load Game", {
+            fontFamily: 'Arial Black', fontSize: 28, color: '#ffffff',
+            stroke: '#000000', strokeThickness: 8,
+            align: 'center'
+        }).setOrigin(0.5);
+
+        this.add.text(512, 420, 'Battlegrounds', {
+            fontFamily: 'Arial Black', fontSize: 28, color: '#ffffff',
             stroke: '#000000', strokeThickness: 8,
             align: 'center'
         }).setOrigin(0.5);
@@ -25,4 +69,9 @@ export class MainMenu extends Scene
 
         });
     }
+    update(){
+        this.bgClouds.tilePositionX += 0.2
+    }
+    
+   
 }
